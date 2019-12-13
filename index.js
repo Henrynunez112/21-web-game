fa97725e0c7847a77bc3c66b042bc9cfe8846f03
Henrynunez112 <henrynunez@pursuit.org>
2019-12-13 18:13:18 -0500

    getting game fuction to work

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
+let deck_id;
+    console.log(deck_id)
 document.addEventListener("DOMContentLoaded",()=>{
-    let deck_id;
     let button = document.querySelector("#startGame");
     button.addEventListener("click",()=>{
-        const fetchAllCards = async ()=>{
-            try{
-                let res = await axios.get(`https://deckofcardsapi.com/api/deck/new/`);
-                deck_id = res.data.deck_id;
-                let shuffled = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
-                deck_id += shuffled.data.deck_id;
-            }catch(error){
-                console.log(error)
-            }
+        let startGame = document.querySelector("#startGame");
+        startGame.style.display = "none";
+        fetchAllCards()
+        game(deck_id);
+        
+    })
+    const fetchAllCards = async ()=>{
+        try{
+            let res = await axios.get(`https://deckofcardsapi.com/api/deck/new/`);
+            let deck_id = res.data.deck_id;
+            let shuffled = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
+            debugger
+            deck_id += shuffled.data.deck_id;
+        }catch(error){
+            console.log(error)
         }
-        const game = async (id) =>{
-            try{
-                debugger
-                let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
-                let playerHand = document.querySelector("#playerHand");
-                for(i = 0; i < drawCards.data.cards.length; i++){
-                    let img = document.createElement("img");
-                    img.src = drawCards.data.cards[i]["image"];
-                    playerHand.appendChild(img)
-                };
-    
-    
-    
-                
-                
-                
-                
-                
-            }
-            catch(error){
-                console.log(error)
-            }
-        }
-        fetchAllCards();
-        game();                
-    })   
+    }
+    const game = async (id) =>{
+        try{
+            let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
+            debugger
+            let playerHand = document.querySelector("#playerHand");
+            for(i = 0; i < drawCards.data.cards.length; i++){
+                let img = document.createElement("img");
+                img.src = drawCards.data.cards[i]["image"];
+                playerHand.appendChild(img)
+            };
 
-    
-    
+
+
+            
+            
+            
+            
+            
+        }
+        catch(error){
+            console.log(error)
+        }
+    }             
 })
 
