c2d684f3e9b2de7ac81cc8492b3c018fe751518c
Henrynunez112 <henrynunez@pursuit.org>
2019-12-13 18:38:23 -0500

    cards drawn

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-let deck_id;
-    console.log(deck_id)
 document.addEventListener("DOMContentLoaded",()=>{
+    let deck_id;
     let button = document.querySelector("#startGame");
     button.addEventListener("click",()=>{
         let startGame = document.querySelector("#startGame");
         startGame.style.display = "none";
-        fetchAllCards()
         game(deck_id);
-        
     })
+    
     const fetchAllCards = async ()=>{
         try{
             let res = await axios.get(`https://deckofcardsapi.com/api/deck/new/`);
-            let deck_id = res.data.deck_id;
+            deck_id = res.data.deck_id;
             let shuffled = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
-            debugger
-            deck_id += shuffled.data.deck_id;
         }catch(error){
             console.log(error)
         }
     }
+    
+    fetchAllCards()
     const game = async (id) =>{
         try{
             let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
-            debugger
+            // debugger
             let playerHand = document.querySelector("#playerHand");
             for(i = 0; i < drawCards.data.cards.length; i++){
                 let img = document.createElement("img");
                 img.src = drawCards.data.cards[i]["image"];
                 playerHand.appendChild(img)
             };
-
-
-
-            
-            
-            
-            
-            
         }
         catch(error){
             console.log(error)
         }
     }             
 })
+
+
+
 
