b28a1dc9a584c9fe45415a6d1f835759dff05785
Henrynunez112 <henrynunez@pursuit.org>
2019-12-17 18:17:09 -0500

    rm all debuggers

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded",()=>{
             let res = await axios.get(`https://deckofcardsapi.com/api/deck/new/`);
             deck_id = res.data.deck_id;
             let shuffled = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
-            debugger
+            
         }catch(error){
             console.log(error)
         }
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded",()=>{
         
         try{
             let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
-            debugger
+            
             for(let i = 0; i < drawCards.data.cards.length; i++){
                 let img = document.createElement("img");
                 img.src = drawCards.data.cards[i]["image"];
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded",()=>{
             h1.innerText = `Players Score: ${total}`
             playerHand.appendChild(h1)
             gameOver();
-            debugger
+            
         }
         catch(error){
             console.log(error)
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded",()=>{
                 scoreArr.push(numTwo)
             }
             score(scoreArr)
-            debugger;
+            ;
             h1.innerHTML = `Player Score: ${total}`;
             playerHand.appendChild(h1)    
             gameOver()
@@ -102,11 +102,11 @@ document.addEventListener("DOMContentLoaded",()=>{
                 imgThree.src = stayBtn.data.cards[i]["image"];
                 let numThree = stayBtn.data.cards[i]["value"]
                 dealerHand.appendChild(imgThree);
-                debugger
+                
                 compScore.push(numThree)
             }
             stayScore(compScore);
-            debugger;
+            ;
             secondH1.innerHTML = `House Score: ${computerScore}`;
             dealerHand.appendChild(secondH1)
             gameOver()
@@ -128,9 +128,6 @@ document.addEventListener("DOMContentLoaded",()=>{
                 if(sum < 11){
                     el = 11;
                     sum += el;
-                }else if(sum === 20){
-                    el = 1;
-                    sum += el
                 }else{
                     el = 1;
                     sum += el
@@ -139,7 +136,7 @@ document.addEventListener("DOMContentLoaded",()=>{
                 sum += Number(el)
             }
         })
-        debugger;
+        ;
         total += sum;
         scoreArr = []
     }
@@ -147,54 +144,51 @@ document.addEventListener("DOMContentLoaded",()=>{
         let sum = 0;
         arr.forEach(el =>{
             if(el === "KING" || el === "QUEEN" || el === "JACK"){
-                debugger
+                
                el = 10;
                sum += el;
             }else if(el === "ACE"){
                 if(sum < 11){
-                    debugger
+                    
                     el = 11;
                     sum += el;
-                }else if(sum === 20){
-                    el = 1;
-                    sum += el
                 }else{
-                    debugger
+                    
                     el = 1;
                     sum += el
                 }
             }else{
-                debugger
+                
                 sum += Number(el)
             }
         })
-        debugger;
+        ;
         computerScore += sum
         compScore = []
     }
 
     const gameOver = () =>{
-        debugger
+        
         if(total > 21){
             h1.innerHTML = `BUSTED!!!${total}`;
             hit.style.display = "none";
             stay.style.display = "none";
             reset.style.display = "block"
             //location.reload()
-            debugger
+            
         }else if(total === 21){
             h1.innerHTML = `BLACKJACK`;
             hit.style.display = "none";
             stay.style.display = "none";
             reset.style.display = "block"           
-            debugger
+            
         }else if(computerScore > 21){
             h1.innerHTML = `The House BUST with ${computerScore}. YOU WIN`;
             secondH1.innerHTML = "";
             stay.style.display = "none";
             hit.style.display = "none";
             reset.style.display = "block"                       
-            debugger
+            
         }
         else if(computerScore === total){
             h1.innerHTML = `ITS A TIE`
@@ -202,7 +196,7 @@ document.addEventListener("DOMContentLoaded",()=>{
             hit.style.display = "none";
             stay.style.display = "none"; 
             reset.style.display = "block"                      
-            debugger
+            
         }
         else if(computerScore > 0 && computerScore > total){
             secondH1.innerHTML = `The house wins ${computerScore}`;
@@ -210,13 +204,13 @@ document.addEventListener("DOMContentLoaded",()=>{
             stay.style.display = "none";
             reset.style.display = "block"
                        
-            debugger         
+                     
         }else if(computerScore > 0 && computerScore < total){
             h1.innerHTML = `Congrats!!!!!!!!! you beat the house with ${total}`;
             hit.style.display = "none";
             stay.style.display = "none";
             reset.style.display = "block"                       
-            debugger
+            
         }
     }
 
